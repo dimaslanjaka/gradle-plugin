e5467779f5cff4274bed5d3b96e7a3088d637d1f
Dimas Lanjaka <dimaslanjaka@gmail.com>
2020-10-14 17:59:41 +0700

    0.6.24 Released - Fix kill java processes task

--- src/main/groovy/com/dimaslanjaka/gradle/core/KillJavaProcesses.groovy
+++ src/main/groovy/com/dimaslanjaka/gradle/core/KillJavaProcesses.groovy
@@ -1,5 +1,6 @@
 package com.dimaslanjaka.gradle.core
 
+import com.dimaslanjaka.gradle.Utils.Utils
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 
@@ -7,8 +8,8 @@ class KillJavaProcesses extends DefaultTask {
     @TaskAction
     void run() {
         def buildDir = getProject().buildDir
-        def classname = this.class.name
-        if (System.getProperty('os.name').toLowerCase(Locale.ROOT).contains('windows')) {
+        def classname = Utils.md5(this.class.name)
+        if (Utils.isWindows) {
             'cmd /k "taskkill /f /im jqs.exe"'.execute(null, new File("$buildDir/tmp/${classname}.log"))
             'cmd /k "taskkill /f /im javaw.exe"'.execute(null, new File("$buildDir/tmp/${classname}.log"))
             'cmd /k "taskkill /f /im java.exe"'.execute(null, new File("$buildDir/tmp/${classname}.log"))
